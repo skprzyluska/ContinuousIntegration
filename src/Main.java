dd36fa36d00fd7805d52e897f2c8216ce1302bd1
skprzyluska <skprzyluska@outlook.com>
2025-06-17 08:44:48 +0200

    Addec a comment

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //
+        // Added a comment
         Wyrażenie jeden, zero, x;
         x = Zmienna.twórz();
         zero = Zero.twórz();
